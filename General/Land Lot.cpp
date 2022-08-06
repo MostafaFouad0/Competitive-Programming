3956451ed13dc0222ac4e1eac8b9daa5e5e7c9db
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2022-08-06 10:21:30 +0200

    Add files via upload

---
+++ General/Land Lot.cpp
@@ -0,0 +1,55 @@
+/*
+ * Created By : Mostafa Mahmoud
+*/
+
+#include<bits/stdc++.h>
+
+#define sareeeee3 ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+using namespace std;
+#define ll long long
+#define ld long double
+const int N = 1e5 + 9;
+void result() {
+    ll n{},m{};
+    cin>>n>>m;
+    ll a[n][m];
+    ll l{},r{};
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin>>a[i][j];
+        }
+    }
+    cin>>l>>r;
+    ll ans=INT64_MAX;
+    for (int op = 0; op <2 ; ++op) {
+        for (int i = 0; i <=n-l; ++i) {
+            for (int j = 0; j <=m-r ; ++j) {
+                ll c{};
+                // calculating all the Possibilities
+                for (int k = 0; k < l; ++k) {
+                    for (int i1 = 0; i1 < r; ++i1) {
+                        c+=a[i+k][j+i1];
+                    }
+                }
+                // choosing the minimum
+                ans=min(ans,c);
+            }
+        }
+        // (lxr) or (rxl)
+        swap(l,r);
+    }
+    cout<<ans;
+
+
+}
+
+
+int main() {
+    sareeeee3
+    ll tc = 1;
+    //cin >> tc;
+    while (tc--) {
+        result();
+        cout << "\n";
+    }
+}
---
+++ General/Rock-paper-scissors.cpp
@@ -0,0 +1,67 @@
+/*
+ * Created By : Mostafa Mahmoud
+*/
+
+#include<bits/stdc++.h>
+
+#define sareeeee3 ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+using namespace std;
+#define ll long long
+#define ld long double
+const int N = 1e5 + 9;
+void result() {
+    string a[3];
+    for (auto & i : a) {
+        cin>>i;
+    }
+    for (int i = 0; i < 3; ++i) {
+        ll c{};
+        for (int j = 0; j < 3; ++j) {
+            if(i!=j){
+                if(a[i]=="rock"&&a[j]!="scissors"){
+                    //can't beat the Jth person so we break (Not possible to beat all of them)
+                    break;
+                }else if(a[i]=="scissors"&&a[j]!="paper"){
+                    // can't beat the Jth person so we break
+                    break;
+                }else if(a[i]=="paper"&&a[j]!="rock"){
+                    //can't beat the Jth person so we break (Not possible to beat all of them)
+                    break;
+                }else {
+                    //can't beat the Jth person so we break (Not possible to beat all of them)
+                    c++;
+                    continue;
+                }
+            }
+        }
+        if(c==2){
+            if(i==0){
+                cout<<"F";
+                return;
+            }
+            if(i==1){
+                cout<<"M";
+                return;
+            }
+            if(i==2){
+                cout<<"S";
+                return;
+            }
+        }
+    }
+    cout<<"?";
+
+
+
+}
+
+
+int main() {
+    sareeeee3
+    ll tc = 1;
+    //cin >> tc;
+    while (tc--) {
+        result();
+        cout << "\n";
+    }
+}
