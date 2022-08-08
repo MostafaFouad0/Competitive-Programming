78f22ff6bb379fb7082b9d6a95c4830c838b9bbd
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2022-08-08 05:54:40 +0200

    Update Bipartite Graph.cpp

--- Templates/Bipartite Graph/Bipartite Graph.cpp
+++ Templates/Bipartite Graph/Bipartite Graph.cpp
@@ -1,17 +1,6 @@
 /*
  * Created By : Mostafa Mahmoud
 */
-
-#include<bits/stdc++.h>
-
-#define sareeeee3 ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
-using namespace std;
-#define ll long long
-#define ld long double
-const int N = 2e5 + 9;
-vector<ll>adj_list[N];
-int vis[N]={0};
-int current_color[N];
 bool BFS_Bipartite(int node){
     // setting the current color of the parent node to 1 (any unique number)
     current_color[node]=1;
@@ -39,54 +28,4 @@ bool BFS_Bipartite(int node){
     }
     // if we reach this then the graph is Bipartite so we return true
     return true;
-}
-
-void result() {
-    ll n{};
-    cin>>n;
-    map<ll,ll>m;
-    bool check= true;
-    for (int i = 0; i <=n; ++i) {
-        adj_list[i].clear();
-    }
-    memset(vis,0,sizeof vis);
-    memset(current_color, -1, sizeof current_color);
-    for (int i = 0; i < n; ++i) {
-        ll u,v;
-        cin>>u>>v;
-        adj_list[u].emplace_back(v);
-        adj_list[v].emplace_back(u);
-        m[u]++,m[v]++;
-        if(m[u]==3||m[v]==3){
-            check=false;
-        }
-    }
-    if(!check){
-        cout<<"NO";
-        return;
-    }
-    for (int i = 1; i <=n; ++i) {
-        if(vis[i]==0){
-            if(!BFS_Bipartite(i)){
-                cout<<"NO";
-                return;
-            }
-        }
-    }
-    cout<<"YES";
-
-
-
-
-}
-
-
-int main() {
-    sareeeee3
-    ll tc = 1;
-    cin >> tc;
-    while (tc--) {
-        result();
-        cout << "\n";
-    }
 }
