9735defd4f25700feb91ef1e2c886166acdca5d2
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2022-08-25 16:38:34 +0200

    Add files via upload

---
+++ General/Grandma Capa Knits a Scarf.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+/*
+*
+* Created By : Mostafa Mahmoud
+*
+*/
+
+#define sareeeee3 ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+using namespace std;
+#define ll long long
+#define F first
+#define S second
+const int N = 1e3 + 9;
+ll MOD = 1e3 + 3;
+#define deb(x) cout<<#x<<"="<<x<<"\n";
+
+
+void result() {
+    ll n{};
+    cin>>n;
+    string s;
+    cin>>s;
+    map<char,ll>m;
+    for (int i = 0; i < s.size(); ++i) {
+        m[s[i]]++;
+    }
+    ll ans=INT64_MAX;
+    for(auto it:m){
+        if(it.second!=0){
+            char tmp=it.first;
+            ll l{},r=n-1,mn{};
+            bool f=false;
+            while(l<=r){
+                if(s[l]==s[r]){
+                    r--;
+                    l++;
+                }else if(s[l]==tmp){
+                    mn++;
+                    l++;
+                }else if(s[r]==tmp){
+                    mn++;
+                    r--;
+                }else{
+                    f=true;
+                    break;
+                }
+            }
+            if(f){
+                continue;
+            }else{
+                ans=min(ans,mn);
+            }
+        }
+    }
+    if(ans==INT64_MAX){
+        cout<<-1;
+        return;
+    }
+    cout<<ans;
+
+
+}
+
+
+int main() {
+    sareeeee3
+    ll tt = 1;
+    cin >> tt;
+    while (tt--) {
+        result();
+        cout << '\n';
+    }
+}
