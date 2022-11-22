b625f6568495f2b3c25d641442274309c0e0e131
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2022-11-22 15:37:25 +0200

    Create A.cpp

---
+++ Codeforces_Contests/Codeforces Round #835 (Div. 4)/A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+/*
+*
+* Created By : Mostafa Mahmoud
+*
+*/
+#define sareeeee3 ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+using namespace std;
+#define ll long long
+#define F first
+#define S second
+const int N = 5e5 + 15;
+#define deb(x) cout<<#x<<"="<<x<<"\n";
+
+
+
+void result() {
+    ll a[3];
+    for (int i = 0; i < 3; ++i) {
+        cin>>a[i];
+    }
+    sort(a,a+3);
+    cout<<a[1];
+
+
+
+
+}
+
+
+int main() {
+    sareeeee3
+    ll tt = 1;
+    cin >> tt;
+    for (int i = 0; i < tt; i++) {
+        result();
+        cout << "\n";
+    }
+}
