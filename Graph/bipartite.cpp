dd6d64223883b3cf3f6a807d24de124c590511f5
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2024-07-30 00:37:20 +0300

    Create bipartite.cpp

---
+++ Graph/bipartite.cpp
@@ -0,0 +1,16 @@
+vector<bool> color;
+
+bool dfs(int u) {
+  // return true if it's bipartite 
+    vis[u] = true;
+    bool f = true;
+    for (auto it: adj[u]) {
+        if (!vis[it]) {
+            color[it] = !color[u];
+            f &= dfs(it);
+        } else {
+            if (color[it] == color[u])f = false;
+        }
+    }
+    return f;
+}
