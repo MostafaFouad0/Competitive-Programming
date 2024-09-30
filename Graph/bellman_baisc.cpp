c667c3bf722ed6ec1d1674081de12e83f73a8126
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2024-09-30 22:24:05 +0300

    Create bellman_baisc.cpp

---
+++ Graph/bellman_baisc.cpp
@@ -0,0 +1,22 @@
+// get shortest path from a specific node and check if there is a negative cycle from this node or not 
+int bellman(int node) {
+    // all elements have cost 1e18
+    cost[node] = 0;
+    int x = -1;
+    for (int i = 0; i < n; ++i) {
+        bool f = false;
+        x = -1;
+        for (auto [u, v, w]: edgs) {
+            if (cost[u] < INT64_MAX) {
+                if (cost[v] > cost[u] + w) {
+                    cost[v] = max((ll) -INT64_MAX, cost[u] + w);
+                    par[v] = u; // to get the path
+                    f = true;
+                    x = v;
+                }
+            }
+        }
+        if (!f)break;
+    }
+    return x; // -1 if there is a negative cycle, else there is no negative cycle
+}
