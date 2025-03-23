9047e5952e28d4d8d9b4ec3892bd8b71778153a5
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2025-03-23 16:05:55 +0200

    Create Tarjan's_algorithm.cpp

---
+++ Templates/Bridges in Graph/Tarjan's_algorithm.cpp
@@ -0,0 +1,26 @@
+int timer{};
+vector<array<int, 2>> bridges;
+void dfs(int u,int par)
+{
+    vis[u] = true;
+    in[u] = low[u] = timer++;
+    for (auto it : adj[u])
+    {
+        if (it == par)continue;
+        if (!vis[it])
+        {
+            dfs(it, u);
+            low[u] = min(low[u], low[it]);
+            if (low[it] > in[u])
+            {
+                /// this a bridge (span edge)
+                bridges.push_back({u, it});
+            }
+        }
+        else
+        {
+            low[u] = min(low[u], low[it]);
+            /// this a back edge
+        }
+    }
+}
